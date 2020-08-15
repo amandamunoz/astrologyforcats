b8653204674e8910a5628ea2913b36902b84de59
Amanda Munoz <amanda.munoz@privy.com>
2020-08-14 20:02:22 -0400

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,29 @@
+<template>
+  <Layout>
+    <h1>astrology for cats</h1>
+
+    <p>
+      meow
+    </p>
+
+    <p class="home-links">
+      <a href="https://instagram.xom/astrologyforcats/" target="_blank" rel="noopener">insta</a>
+      <a href="https://github.com/amandamunoz/astrologyforcats" target="_blank" rel="noopener">contribute to this project</a>
+    </p>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'astrology for cats'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
